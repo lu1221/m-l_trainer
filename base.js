945d60c8863af5a7975952ab48b1ba8dd435f6d5
shlu <shang.lu@mail.utoronto.ca>
2018-11-16 00:53:44 -0500

    working game base

--- base.js
+++ base.js
@@ -10,6 +10,7 @@ var GameState = function(game) {
 GameState.prototype.preload = function() {
     this.game.load.spritesheet('ship', '/assets/gfx/ship.png', 32, 32);
     this.game.load.image('ground', '/assets/gfx/ground.png');
+    this.game.load.spritesheet('explosion', '/assets/gfx/explosion.png', 128, 128);
 };
 
 // Setup the example
@@ -21,11 +22,11 @@ GameState.prototype.create = function() {
     this.ROTATION_SPEED = 180; // degrees/second
     this.ACCELERATION = 200; // pixels/second/second
     this.MAX_SPEED = 250; // pixels/second
-    this.DRAG = 25; // pixels/second
-    this.GRAVITY = 100; // pixels/second/second
+    this.DRAG = 0; // pixels/second
+    this.GRAVITY = 50; // pixels/second/second
 
     // Add the ship to the stage
-    this.ship = this.game.add.sprite(this.game.width/2, this.game.height/2, 'ship');
+    this.ship = this.game.add.sprite(0, 0, 'ship');
     this.ship.anchor.setTo(0.5, 0.5);
     this.ship.angle = -90; // Point the ship up
 
@@ -37,6 +38,9 @@ GameState.prototype.create = function() {
 
     // Add drag to the ship that slows it down when it is not accelerating
     this.ship.body.drag.setTo(this.DRAG, this.DRAG); // x, y
+
+    // Choose a random starting angle and velocity for the ship
+    this.resetShip();
 
     // Turn on gravity
     game.physics.arcade.gravity.y = this.GRAVITY;
@@ -55,6 +59,9 @@ GameState.prototype.create = function() {
         this.ground.add(groundBlock);
     }
 
+    // Create a group for explosions
+    this.explosionGroup = this.game.add.group();
+
     // Capture certain keys to prevent their default actions in the browser.
     // This is only necessary because this is an HTML5 game. Games on other
     // platforms may not need code like this.
@@ -64,6 +71,57 @@ GameState.prototype.create = function() {
         Phaser.Keyboard.UP,
         Phaser.Keyboard.DOWN
     ]);
+};
+
+// Try to get a used explosion from the explosionGroup.
+// If an explosion isn't available, create a new one and add it to the group.
+// Setup new explosions so that they animate and kill themselves when the
+// animation is complete.
+GameState.prototype.getExplosion = function(x, y) {
+    // Get the first dead explosion from the explosionGroup
+    var explosion = this.explosionGroup.getFirstDead();
+
+    // If there aren't any available, create a new one
+    if (explosion === null) {
+        explosion = this.game.add.sprite(0, 0, 'explosion');
+        explosion.anchor.setTo(0.5, 0.5);
+
+        // Add an animation for the explosion that kills the sprite when the
+        // animation is complete
+        var animation = explosion.animations.add('boom', [0,1,2,3], 60, false);
+        animation.killOnComplete = true;
+
+        // Add the explosion sprite to the group
+        this.explosionGroup.add(explosion);
+    }
+
+    // Revive the explosion (set it's alive property to true)
+    // You can also define a onRevived event handler in your explosion objects
+    // to do stuff when they are revived.
+    explosion.revive();
+
+    // Move the explosion to the given coordinates
+    explosion.x = x;
+    explosion.y = y;
+
+    // Set rotation of the explosion at random for a little variety
+    // explosion.angle = this.game.rnd.integerInRange(0, 360);
+
+    // Play the animation
+    explosion.animations.play('boom');
+
+    // Return the explosion itself in case we want to do anything else with it
+    return explosion;
+};
+
+GameState.prototype.resetShip = function() {
+    // Move the ship back to the top of the stage
+    this.ship.x = this.game.width/2;
+    this.ship.y = 32;
+    this.ship.body.acceleration.setTo(0, 0);
+
+    // Select a random starting angle and velocity
+    this.ship.body.velocity.setTo(0, 0);
 };
 
 // The update() method is called every frame
@@ -84,6 +142,25 @@ GameState.prototype.update = function() {
     } else {
         // Stop rotating
         this.ship.body.angularVelocity = 0;
+    }
+
+    // Set a variable that is true when the ship is touching the ground
+    var onTheGround = this.ship.body.touching.down;
+
+    if (onTheGround) {
+        if (Math.abs(this.ship.body.velocity.y) > 20 || Math.abs(this.ship.body.velocity.x) > 30) {
+            // The ship hit the ground too hard.
+            // Blow it up and start the game over.
+            this.getExplosion(this.ship.x, this.ship.y);
+            this.resetShip();
+        } else {
+            // We've landed!
+            // Stop rotating and moving and aim the ship up.
+            this.ship.body.angularVelocity = 0;
+            this.ship.body.velocity.setTo(0, 0);
+            this.ship.angle = -90;
+        }
+
     }
 
     if (this.upInputIsActive()) {
