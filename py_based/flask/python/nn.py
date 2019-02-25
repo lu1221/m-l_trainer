739ae3cb22ddec4f6bd1c822e42626df7e4c920b
erfdev <erfsnet@gmail.com>
2019-02-24 20:11:03 -0500

    Added function to return prediction matrix for all combinations of inputs that result in output=1.

--- py_based/flask/python/nn.py
+++ py_based/flask/python/nn.py
@@ -1,22 +1,30 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+import numpy as np
 import random
 
-NUM_FEATURES = 3
-NUM_TRAIN_SETS = 100
+NUM_FEATURES = 2
+NUM_TRAIN_SETS = 10000
+PRECISION = 1 #Number of decimal places for precision
 
-#### X Inputs will be array of values (y-coordinate, velocity, acceleration) ###
+
+MIN_DIST = 0.0
+MAX_DIST = 402.0
+
+MIN_VEL = -250.0
+MAX_VEL = 250.0
+
+#### X Inputs will be array of values (y-coordinate, velocity) ###################################
 #
-# [y]            coordinates in base.js games ranges from 0 (ceiling) to 402 (ground)
-# [velocity]     velocity ranges from -250 (max speed going up) to 250 (max fall speed possible)
-# [acceleration] acceleration is BINARY, either GRAVITY=50 or ACCELERATION=200
-#                TODO: Maybe set this to -1 for GRAVITY and 1 for ACCELERATION ?
+# [distance]            coordinates in base.js games ranges from 0.0 (ceiling) to 402.0 (ground)
+# [velocity]     velocity ranges from -250.0 (max speed going up) to 250.0 (max fall speed possible)
 # TODO: Consider extra features for expanding to horizontal movement of lander
-################################################################################
+# [boost]        binary output 1=boost, 0=no action
+##################################################################################################
 
-# Setup a randomized neural network
+# Setup a randomized neural network with 2 hidden layers of size 5,5
 def initRandNN():
-  mlp = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+  mlp = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(5, 5), random_state=1)
   return mlp
 
 
@@ -27,14 +35,13 @@ def generateRandFeatures():
   for i in range(NUM_TRAIN_SETS):
     X.append([])
     for j in range(NUM_FEATURES):
-        if(j == 0):
-            X[i].append(random.randint(0,402));
-        if(j == 1):
-            X[i].append(random.randint(-250,250));
-        if(j == 2):
-            X[i].append(-50 if random.randint(0,1) == 0 else 200)
+        if(j == 0): #distance
+            X[i].append(round(random.uniform(MIN_DIST,MAX_DIST),PRECISION));
+        if(j == 1): #vel
+            X[i].append(round(random.uniform(MIN_VEL,MAX_VEL),PRECISION));
   return X
 
+#Generate randomized output
 def generateRandOutputs():
    Y = []
    for i in range(NUM_TRAIN_SETS):
@@ -45,6 +52,73 @@ def printFeatures(X):
   for r in range(len(X)):
     print(X[r])
 
+def printNNInfo(mlp):
+  print("******************* MLP Info: *************************")
+  for i in range(len(mlp.coefs_)):
+      number_neurons_in_layer = mlp.coefs_[i].shape[1]
+      for j in range(number_neurons_in_layer):
+          weights = mlp.coefs_[i][:,j]
+          print(i, j, weights, end=", ")
+          print()
+      print()
+  
+  print("Bias values for first hidden layer:")
+  print(mlp.intercepts_[0])
+  print("\nBias values for second hidden layer:")
+  print(mlp.intercepts_[1])
+  print("*******************************************************")
+
+
+
+# Using a FITTED MLP
+# Returns prediction matrix with 2 columns (dist, vel) for all values where output boost = 1
+# For PRECISION value of 1:
+#    4020 possible distance values
+#    5000 possible velocity values
+def getPredictionMatrix(mlp):
+  num_rows = (((MAX_DIST-MIN_DIST)*(MAX_VEL-MIN_VEL))/(PRECISION*.1)**2)
+  print("Number of rows for prediction matrix is ",int(num_rows))
+  X1=np.array(np.arange(MIN_DIST,MAX_DIST,PRECISION*.1))
+  X2=np.array(np.arange(MIN_VEL,MAX_VEL,PRECISION*.1))
+  print(X1)
+  print(X2)
+  index = 0
+  X = np.zeros((int(num_rows),2))
+  print("X Array Shape:",X.shape)
+  #print(np.linspace(MIN_DIST,MAX_DIST,(MAX_DIST-MIN_DIST)/(PRECISION*.1),endpoint=False))
+  for i in np.arange(0,402,0.1):
+    for j in np.arange(-250,250,0.1):
+      #print(index, i, j)
+      X[index] = np.array([i,j])
+      index = index + 1
+      if(index > num_rows-1):
+        break
+  #print(X)
+  #print(mlp.predict(X))
+  OutPut1 = np.array(mlp.predict(X))
+  print("Number of samples where prediction=1:",np.sum(OutPut1))
+  index_first1 = np.nonzero(OutPut1==1)[0][0]
+  print("First index of prediction=1",index_first1, X[index_first1])
+  index_predictions_1 = np.nonzero(OutPut1==1)
+  print("Indices of prediction=1: ",index_predictions_1)
+  #print("Prediction:",mlp.predict(np.array(X[index_first1]).reshape(1, -1)))
+
+  PredictArray = np.zeros((np.sum(OutPut1),2))
+  print("PredictArrayShape:",PredictArray.shape)
+  index = 0
+  for i in range(np.sum(OutPut1)):
+    #print(i)
+    #print(index_predictions_1[0][i])
+    #print(X[index_predictions_1[0][i]])
+    PredictArray[i]= X[index_predictions_1[0][i]]
+  print(PredictArray)
+  print(mlp.predict(PredictArray))
+  return PredictArray
+
+  
+
+
+
 #################################################################################
 # Just trying out functions here to see if they are working                     #
 #################################################################################
@@ -54,9 +128,8 @@ mlp = initRandNN()
 rand_X = generateRandFeatures()
 rand_Y = generateRandOutputs()
 print(mlp)
-printFeatures(rand_X)
-print(rand_Y)
-
+#printFeatures(rand_X)
+#print(rand_Y)
 
 ##Standardize features by removing the mean and scaling to unit variance
 ##The standard score of a sample x is calculated as:
@@ -67,5 +140,17 @@ print(rand_Y)
 #print(scaler.transform(rand_X))
 mlp.fit(rand_X, rand_Y)
 
-predict_X = generateRandFeatures()
-print(mlp.predict(predict_X))
+print([coef.shape for coef in mlp.coefs_])
+print (mlp.coefs_)
+
+printNNInfo(mlp)
+
+
+
+PredictionMatrix = getPredictionMatrix(mlp)
+
+#predict_X = generateRandFeatures()
+#print(mlp.predict(predict_X))
+
+#arr = np.arange(0,11)
+#print(arr)
